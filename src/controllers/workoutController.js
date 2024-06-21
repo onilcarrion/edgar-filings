6d40e97c0827797c271c844d75464d904556f95e
onilcarrion <onil@owlin.com>
2024-06-21 13:35:51 +0200

    [Basic-Setup]-JSON format

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,28 +1,36 @@
-// In src/controllers/workoutController.js
+// In src/controllers/workoutControllers.js
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = (req, res) => {
-    res.send("Get all workouts");
-  };
-  
-  const getOneWorkout = (req, res) => {
-    res.send("Get an existing workout");
-  };
-  
-  const createNewWorkout = (req, res) => {
-    res.send("Create a new workout");
-  };
-  
-  const updateOneWorkout = (req, res) => {
-    res.send("Update an existing workout");
-  };
-  
-  const deleteOneWorkout = (req, res) => {
-    res.send("Delete an existing workout");
-  };
-  
-  module.exports = {
-    getAllWorkouts,
-    getOneWorkout,
-    createNewWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
-  };
+  const allWorkouts = workoutService.getAllWorkouts();
+  // *** ADD ***
+  res.send({ status: "OK", data: allWorkouts });
+};
+
+const getOneWorkout = (req, res) => {
+  const workout = workoutService.getOneWorkout();
+  res.send("Get an existing workout");
+};
+
+const createNewWorkout = (req, res) => {
+  const createdWorkout = workoutService.createNewWorkout();
+  res.send("Create a new workout");
+};
+
+const updateOneWorkout = (req, res) => {
+  const updatedWorkout = workoutService.updateOneWorkout();
+  res.send("Update an existing workout");
+};
+
+const deleteOneWorkout = (req, res) => {
+  workoutService.deleteOneWorkout();
+  res.send("Delete an existing workout");
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
