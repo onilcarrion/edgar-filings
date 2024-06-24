722c7c2567b0a10455be79a20dad7c75d25bc470
onilcarrion <onil@owlin.com>
2024-06-24 10:04:28 +0200

    [Basic-Setup]-Data Caching

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-// In src/index.js 
+// In src/index.js
 const express = require("express");
-// *** ADD ***
 const bodyParser = require("body-parser");
+// *** ADD ***
+const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
+// *** ADD ***
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
-// *** ADD ***
 app.use(bodyParser.json());
+// *** ADD ***
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
