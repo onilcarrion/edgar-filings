e08dfb2bb4d2a7d8db8c79f2d215bdeb1c416c5a
onilcarrion <onil@owlin.com>
2024-06-24 10:40:19 +0200

    [Basic-Setup]-Swagger Implementation (Documenting API)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 // In src/index.js
 const express = require("express");
 const bodyParser = require("body-parser");
-// *** ADD ***
-const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+// *** ADD ***
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
-// *** ADD ***
-const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-// *** ADD ***
-app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  /// *** ADD ***
+  V1SwaggerDocs(app, PORT);
 });
